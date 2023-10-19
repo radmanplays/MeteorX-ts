bbd5911fb3f431df4c2bc164c2ab72522973cfdd
OtterDev <144633084+OtterCodes102@users.noreply.github.com>
2023-10-19 09:58:13 -0600

    add prefix to commands

--- src/cmds.ts
+++ src/cmds.ts
@@ -1,4 +1,6 @@
 import { uwuify } from "./uwuapi";
+import { MeteorXlog } from "./chatutils";
+
 var version = "v1.0";
 var cmds = ".version , .help , .ip , .uwuify"
 var serverip = null;
@@ -10,23 +12,22 @@ export function registercmds() {
     //@ts-ignore
     PluginAPI.addEventListener("sendchatmessage", function(event) {
         if (event.message === ".version") {
-                //@ts-ignore
-            PluginAPI.displayToChat({ msg: "you are currently using the " + version + " version of MeteorX." });
+            MeteorXlog("you are currently using the " + version + " version of MeteorX.");
             event.preventDefault = true;
         }
         if (event.message === ".help") {
             //@ts-ignore
-            PluginAPI.displayToChat({ msg: "MeteorX commands : " + cmds + " " });
+            MeteorXlog("MeteorX commands : " + cmds + " ");
             event.preventDefault = true;
         }
         if (event.message === ".ip") {
             //@ts-ignore
-            PluginAPI.displayToChat({ msg: "the ip of the current server you are playing: " + serverip + " " });
+            MeteorXlog("the ip of the current server you are playing: " + serverip + " ");
             event.preventDefault = true;
         }
         if (event.message === ".uwuify") {
             //@ts-ignore
-            PluginAPI.displayToChat({ msg: "usage: .uwuify [message]"});
+            MeteorXlog("usage: .uwuify [message]");
             event.preventDefault = true;
         }
         if (event.message.startsWith(".uwuify ")) {
