88cf00e58cf6828f846a79994959f2aaeeb4079c
radmanplays <95340057+radmanplays@users.noreply.github.com>
2023-11-08 14:29:12 +0330

    Update cmds.ts

--- src/cmds.ts
+++ src/cmds.ts
@@ -78,5 +78,42 @@ export function registercmds() {
             MeteorXlog("your last reported cords(IRL(jk)): " + " x= " + playerx + " y= " + playery + " z= " + playerz);
             event.preventDefault = true;
         }
+        if (event.message === ".vclip") {
+            MeteorXlog("usage: .vclip [value/blocks]");
+            event.preventDefault = true;
+        }
+        if (event.message.startsWith(".vclip ")) {
+            //from https://github.com/MeteorDevelopment/meteor-client/blob/master/src/main/java/meteordevelopment/meteorclient/commands/commands/VClipCommand.java
+            //@ts-ignore
+            var targetLength = ".vclip".length;
+            var message = event.message.toString().trim();
+            if ( //If
+                message.length > targetLength && //The length of the message is longer than the target length
+                message.substring(0, targetLength).trim().toLowerCase() === ".vclip" //And, the content from character 0 (first) to that of the length, trimmed and put to lowercase is ".makescroll"
+              ) {
+                var vclipvalue = message.substring(targetLength+1);
+                //@ts-ignore
+                var isplayerriding = PluginAPI.player.isRiding()
+                var packetsrequired = Math.ceil(Math.abs(vclipvalue / 10));
+                if (packetsrequired > 20) {
+                    packetsrequired = 1;
+                }
+                if (isplayerriding === false){
+                    // For each 10 blocks, send a player move packet with no delta
+                    for (var packetnumber = 0; packetnumber < (packetsrequired - 1) packetnumber++) {
+                        //@ts-ignore
+                        PluginAPI.network.sendPacketPlayerPosition({onGround: true})  
+                    }
+                    // Now send the final player move packet
+                    PluginAPI.network.sendPacketPlayerPosition({onGround: true, x: getplayerpos.x, y: getplayerpos.y + vclipvalue, z: getplayerpos.z})
+                    PluginAPI.player.setPosition(x: getplayerpos.x, y: getplayerpos.y + vclipvalue, z: getplayerpos.z)
+                    MeteorXsuccess("successfully vclipped " + vclipvalue + "blocks!")
+                }
+                if (isplayerriding === true){
+                    MeteorXerror("you need to get off this mob/vehicle to use this command!")
+                }
+            }
+            event.preventDefault = true;
+        }
     });
 }
