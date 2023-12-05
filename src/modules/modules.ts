4d0377fd7a46551a14a1f6687b0c950445110d9e
OtterDev <144633084+OtterCodes102@users.noreply.github.com>
2023-12-05 08:41:47 -0700

    Update modules.ts

--- src/modules/modules.ts
+++ src/modules/modules.ts
@@ -20,7 +20,7 @@ export function registermodule(module: Module): void {
           </tr>
         `
         */
-        eval(module.name+'gui.innerHTML = `<th style="text-align: center; width: 15%;">Activate/Deactivate</th></tr><tr style="box-shadow: grey 0px 2px 0px;"><td style="user-select: text;background-color: #9d00ff30;">' + module.name + '</td><td style="background-color: gray;text-align: center;cursor: pointer;user-select: none;" id="' + module.name '">Activate</td></tr>`')
+        eval(module.name+'gui.innerHTML = `<th style="text-align: center; width: 15%;">Activate/Deactivate</th></tr><tr style="box-shadow: grey 0px 2px 0px;"><td style="user-select: text;background-color: #9d00ff30;">${module.name}</td><td style="background-color: gray;text-align: center;cursor: pointer;user-select: none;" id="${module.name}">Activate</td></tr>`')
         eval(`hacks.appendChild(${module.name+"gui"});`)
       } else {
         throw Error("[MeteorX] Error: module name not declared")
