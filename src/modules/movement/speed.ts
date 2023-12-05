4bc68f6f4db2a0d930cd1ff598f2c40868d1b418
radmanplays <95340057+radmanplays@users.noreply.github.com>
2023-12-05 13:27:50 +0000

    fixed speed

--- src/modules/movement/speed.ts
+++ src/modules/movement/speed.ts
@@ -1,22 +1,29 @@
 let speedactive: boolean = false
 let speedvalue : number = 2
+let pre = true
 import { returnisjetpackflying, returnjetpacktoggle } from "./jetpack";
 export function registerspeed(){
     //@ts-ignore
     PluginAPI.require("player")
     //@ts-ignore
-    var prex = PluginAPI.player.motionX
-    //@ts-ignore
-    var prez = PluginAPI.player.motionZ
-    //@ts-ignore
     PluginAPI.addEventListener("update", ()=>{
+        if (speedactive == false){
+            //@ts-ignore
+            var prex = PluginAPI.player.motionX
+            //@ts-ignore
+            var prez = PluginAPI.player.motionZ
+            pre = true
+        }
         //@ts-ignore
         if (PluginAPI.player.onGround){
             if (speedactive == true){
+                pre = false
                 //@ts-ignore
                 PluginAPI.player.motionX *= speedvalue
                 //@ts-ignore
                 PluginAPI.player.motionZ *= speedvalue
+                //@ts-ignore
+                PluginAPI.player.reload()
             }
         } else if (returnisjetpackflying()&&returnjetpacktoggle){ // this makes flying with jetpack more fun :D (and faster)
             if (speedactive == true){
@@ -24,13 +31,17 @@ export function registerspeed(){
                 PluginAPI.player.motionX *= speedvalue
                 //@ts-ignore
                 PluginAPI.player.motionZ *= speedvalue
+                //@ts-ignore
+                PluginAPI.player.reload()
             }
         }
-        if (speedactive == false){
+        if (pre == true){
             //@ts-ignore
             PluginAPI.player.motionX = prex
             //@ts-ignore
             PluginAPI.player.motionZ = prez
+            //@ts-ignore
+            PluginAPI.player.reload()
         }
     });
 }
